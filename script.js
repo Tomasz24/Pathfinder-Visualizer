5e19af177d0d2095fbbf9f2e7e069be4f8a01dd6
Tomasz <peszkoto@gmail.com>
2022-09-02 03:09:54 -0400

    Connect nodes with edges

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ function arrayTest(id) {
 function addNode(graph, node) {
     // update node array
     graph.nodes.push(node);
-    let n = graph.nodes.length;  // number of nodes
-    node.idx = n;
+    let n = graph.nodes.length;     // number of nodes
+    node.idx = n - 1;               // since indexing starts at 0
 
     // update adjacency matrix
     array = [0];
@@ -94,6 +94,45 @@ function printNodes() {
     for (let i=0; i<n; i++) {
         console.log(myGraph.nodes[i])
     }
+}
+
+
+function addEdge(id1, id2) {
+    // get names of nodes
+    let nodeName1 = document.getElementById(id1).value;
+    let nodeName2 = document.getElementById(id2).value;
+    // get number of nodes
+    let n = myGraph.nodes.length;
+    // declare nodes so accessible throughout the function
+    let node1 = null;
+    let node2 = null;
+
+    // go through and find nodes in myGraph
+    for (let i=0; i<n; i++) {
+        // check if node1
+        if (myGraph.nodes[i].name == nodeName1) {
+            node1 = myGraph.nodes[i];
+        }
+        // check if node2
+        if (myGraph.nodes[i].name == nodeName2) {
+            node2 = myGraph.nodes[i];
+        }
+        // break out if nodes have been found
+        if (node1 && node2) break;
+    }
+
+    console.log("Before:");
+    console.log(printArray(myGraph.adjMtx));
+    // modify adjMtx
+    myGraph.adjMtx[node1.idx][node2.idx] = 1;
+    myGraph.adjMtx[node2.idx][node1.idx] = 1;
+    console.log("After:");
+    console.log(myGraph.adjMtx);
+}
+
+
+function adjMtxTest() {
+    printArray(myGraph.adjMtx);
 }
 
 
